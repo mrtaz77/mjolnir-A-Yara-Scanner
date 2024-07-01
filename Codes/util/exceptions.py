b502af60e23679016a50681b1ed667702550cfd4
mrtaz77 <mrtazwar23@gmail.com>
2024-07-01 19:46:39 +0600

    Move exceptions to separate file

---
+++ Codes/util/exceptions.py
@@ -0,0 +1,11 @@
+class FileNotFoundError(Exception):
+	pass
+
+class InvalidFileExtensionError(Exception):
+	pass
+
+class NoYaraFilesFoundError(Exception):
+	pass
+
+class EmptyPathError(Exception):
+	pass
